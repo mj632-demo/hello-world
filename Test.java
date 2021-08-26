b484b7882cb8b50c9422211245af29899d8b67f0
mj632-demo <83748532+mj632-demo@users.noreply.github.com>
2021-08-26 09:36:11 -0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,23 @@
 class Test {
-public static void main(String[] arg) {
-String s = "hello";
-}
+  public static void main(String[] arg) {
+    String s = "hello";
+  }
+  public void bar()
+  {
+      System.out.println( "Hello World!" );
+  }
+
+  private void foo(String p) {
+     String s = "aloha";
+     System.out.println(String.format("This is a faulty message: %i", s));
+     System.out.format("No %s",1);
+     if ("1" == "2") {
+          //dosomething
+     }
+  }
+
+  private void cipher(String s) throws Exception
+  {
+      Cipher c = Cipher.getInstance(s);
+  }
 }
